9610390fe911dd92fa6808c1994351d22947cd60
Bem.Zheng <breezescut@gmail.com>
2017-02-25 18:58:18 +0800

    remove c++ source

--- Python/Amy_Python/lesson1.py
+++ Python/Amy_Python/lesson1.py
@@ -5,12 +5,6 @@ def hello_world(xx, ddd):
     print("Hello world")
     return xx
 
-
-#void hello_world(int xx, char xx){
- #   cout<<"hello world"<<endl;
- #   return "hh"
-#}
-
 if __name__ == "__main__":
     x = hello_world("ssssd", "sss")
     print(x)
