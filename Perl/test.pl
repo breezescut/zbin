afa06a6f52d01d5c3adb6787dd8f7e56c2d6eb28
root <bem.zheng@ericsson.com>
2017-03-23 15:46:03 +0800

    1. test.pl 增加心跳支持 2. test_config.pl 增加開門事件支持

--- Perl/test.pl
+++ Perl/test.pl
@@ -4,12 +4,14 @@ require 'test_config.pl';
 use IO::Socket;
 use JSON;
 use Data::Dumper;
-#use IO::Socket::Timeout;
+use IO::Socket::Timeout;
+use Net::MQTT::Simple;
 use utf8;
 #use utf8::all;
 
 binmode STDOUT, ":utf-8";
 binmode STDERR, ":utf-8";
+
 
 
 #my $speecher_devid="3095e3020743";
@@ -22,6 +24,7 @@ my $robot_devid = "robottesting";
 my $type = 0;
 my $addr = $ARGV[0] || '127.0.0.1';
 my $port = $ARGV[1] || '7004';
+my $mqtthost = $ARGV[2] || '47.93.80.117:1883';
 #my $output = $ARGV[2] || 'result.txt';
 
 
@@ -32,15 +35,8 @@ my $sock = IO::Socket::INET->new(
         Proto    => 'tcp')
         or die "Can't connect: $!\n";
 
-my $robot;
+my $m = Net::MQTT::Simple->new($mqtthost) || die("Can't connect mqtt $mqtthost:$!\n");
 
-if ($type == 1) { 
-        $robot = IO::Socket::INET->new(
-        PeerAddr => $addr,
-        PeerPort => $port,
-        Proto    => 'tcp')
-        or die "Can't connect: $!\n";
-}
 
 
 my $first_message = qq|{"devid":"$speecher_devid", "devicetype":"60", "reqtype":"sendid", "status":"", "content":""}|;
@@ -55,9 +51,29 @@ my $json = JSON->new->utf8;
 
 my $format = qq|{"devid":"$speecher_devid", "devicetype":"60", "reqtype":"sendword", "status":"1", "content":"%s"}|;
 
+my $pid = 0;
+if ($pid = fork() > 0) { 
+} elsif($pid == 0)  { 
+
+        while (1) {
+                my $heartbeat = qq|{"devicetype":"60","devid":"020000000000","reqtype":"heartbeat"}|;
+                my $r = $sock->send(${heartbeat});
+
+                sleep(20);
+        }
+        exit(0);
+}
+
+END{  
+        if (defined($pid) && $pid > 0) { 
+                kill(9, $pid);
+        }
+} 
+
+
 
 while (1) { 
-	print "Input option:";
+        print "Input option:";
         $one = <STDIN>;
         chomp($one);
         $one =~ s/[ \t]+//g;
@@ -68,6 +84,9 @@ while (1) {
                 my $msg= sprintf($format, $config{$one}) ;
                 print $msg."\n";
                 my $r = $sock->send($msg);
+        } elsif (defined($mqttconfig{$one})) { 
+                print $mqttconfig{$one}."\n";
+                my $r = $m->publish("c2s/device/50294D20A98F", $mqttconfig{$one});
         } else { 
                 print "unkown command\n";
         }
