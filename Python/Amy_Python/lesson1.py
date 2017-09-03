036f51b482562011b66b1be524320758761a4dc9
Bem Zheng <zbinscut@gmail.com>
2017-09-03 21:16:28 +0800

    New Pc Test2

--- Python/Amy_Python/lesson1.py
+++ Python/Amy_Python/lesson1.py
@@ -7,7 +7,8 @@ def hello_world(xx, ddd):
 if __name__ == "__main__":
     x = hello_world("ssssd", "sss")
     print(x)
-    ss = input("input your name:")
-    print("Hello %s" % ss)
+    # ss = input("input your name:")
+    # print("Hello %s" % ss)
     print("hello_world")
     print("hello_world2")
+    print("hello world3")
