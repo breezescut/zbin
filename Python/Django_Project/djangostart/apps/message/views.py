11ee06ec2abf324ec193f2ebf9f30b6a1f7cc710
Bem Zheng <zbinscut@gmail.com>
2017-11-27 10:05:26 +0800

    提交mysite 项目代码

--- Python/Django_Project/djangostart/apps/message/views.py
+++ Python/Django_Project/djangostart/apps/message/views.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import MySQLdb
 
 # Create your views here.
+
+def getform(request):
+    return render(request, 'message_form.html')
+
+def book_list(request):
+    db = MySQLdb.connect(user='me', db='mydb', passwd='secret', host='localhost')
+    cursor = db.cursor()
+    cursor.execute('select name from books ORDER by name')
+    names = [ row[0] for row in cursor.fetchall()]
+    db.close()
